e8c78d89ce63b653a3e9d3187e2e2c68466079ce
Erin Loy <eloy@crazydogventures.com>
2023-03-26 11:52:11 -0700

    Revert "wip"
    This reverts commit 99c554055299eb02c0e4fcd346649548ab134007.

--- samples/TodoApp/Host/HostSettings.cs
+++ samples/TodoApp/Host/HostSettings.cs
@@ -6,10 +6,10 @@ public class HostSettings
     public bool UseInMemoryAuthService { get; set; } = false;
 
     // DBs
-    public string UseSqlServer { get; set; } = //"";
-        "Data Source=localhost;Initial Catalog=fusion_blazorise_template;Integrated Security=True;TrustServerCertificate=True;";
-    public string UsePostgreSql { get; set; } = "";
-    //"Server=localhost;Database=stl_fusion_todoapp_{0:StorageId};Port=5432;User Id=postgres;Password=postgres";
+    public string UseSqlServer { get; set; } = "";
+        // "Data Source=localhost;Initial Catalog=fusion_blazorise_template;Integrated Security=False;User ID=sa;Password=SqlServer1";
+    public string UsePostgreSql { get; set; } =
+        "Server=localhost;Database=stl_fusion_todoapp_{0:StorageId};Port=5432;User Id=postgres;Password=postgres";
 
     public string MicrosoftAccountClientId { get; set; } = "6839dbf7-d1d3-4eb2-a7e1-ce8d48f34d00";
     public string MicrosoftAccountClientSecret { get; set; } =
